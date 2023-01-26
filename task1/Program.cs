6ce3c689406286fea00a0f1e1c7feb97e1e6cb29
Antonik Nikita <n_antonik@mail.ru>
2023-01-26 12:08:28 +0300

    задачки

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number1 / number2;
+
+if (result == number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");  
+}
---
+++ task2/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToString(Console.ReadLine());

26410a8db08ec6d9a345ea71e815baeab5996ee8
Antonik Nikita <n_antonik@mail.ru>
2023-02-02 10:42:47 +0300

    massive

--- massive/Program.cs
+++ massive/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿string name1 = "Bob";
+string name2 = "Sara";
+string name3 = "Mary";
+string name4 = "John";
+string name5 = "Ann";
+
+string[] names = {name1, "Sara", name3, name4, name5};
+
+Console.WriteLine(names[2]);
+names[0] = "Andry";
+Console.WriteLine(names[0]);
+
+int[] nums = {1, 3, 5, 7};
+// int[] nums = new int[4] {1, 3, 5, 7};
+// int[] nums = new int[] {1, 3, 5, 7};
+// int[] nums = new[] {1, 3, 5, 7};
+// int[] nums = new int[4];
+
+Console.WriteLine(names.Length);
+// Console.WriteLine(names[names.Length]);
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+
+foreach (int item  in nums)
+{
+     Console.WriteLine(item);
+}

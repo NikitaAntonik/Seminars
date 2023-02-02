c9a2237c7f152801123b263c761a9b13ba9d07de
Antonik Nikita <n_antonik@mail.ru>
2023-02-02 12:04:25 +0300

    задачи на семинаре

--- task2/Program.cs
+++ task2/Program.cs
@@ -10,15 +10,15 @@ if (x > 0 & y > 0)
 {
     Console.WriteLine("четверть 1");
 }
-else if (x > 0 & y > 0)
+else if (x < 0 & y > 0)
 {
     Console.WriteLine("четверть 2");
 }
-else if (x > 0 & y > 0)
+else if (x < 0 & y < 0)
 {
     Console.WriteLine("четверть 3");
 }
-else if (x > 0 & y > 0)
+else if (x > 0 & y < 0)
 {
     Console.WriteLine("четверть 4");
 }
---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 21) Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
+//  в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Write("Введите координаты первой точки (xa ≠ 0 и ya ≠ 0): ");
+int xa = Convert.ToInt32(Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты второй точки (xb ≠ 0 и yb ≠ 0): ");
+int xb = Convert.ToInt32(Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
+
+//double dist = Math.Sqrt((xb-xa)^2 + (yb-ya)^2);
+int x = xb - xa;
+int y = yb -ya;
+double length = Math.Pow(x,2);
+double length1 = Math.Pow(y,2);
+double dist = Math.Sqrt(length + length1);
+Console.Write(dist);
+

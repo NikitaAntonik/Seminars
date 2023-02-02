f9930895f7728e4cf7499c3842399577d324d341
Antonik Nikita <n_antonik@mail.ru>
2023-02-02 11:19:11 +0300

    задачи на семинаре

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите номер четверти: ");
+int xy = Convert.ToInt32(Console.ReadLine());
+if (xy == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+else if (xy == 2)
+{
+        Console.WriteLine("x < 0 и y > 0");
+    }
+    else if (xy == 3)
+    {
+        Console.WriteLine("x < 0 и y < 0");
+    }
+    else if (xy == 4)
+    {
+        Console.WriteLine("x > 0 и y < 0"); 
+    }
+    else{
+        Console.WriteLine("Такой четверти не существует.");
+    }

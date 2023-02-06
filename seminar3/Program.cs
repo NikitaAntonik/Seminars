a71b7099d99a8c456e2e166fbebe83e28f470031
Antonik Nikita <n_antonik@mail.ru>
2023-02-06 12:04:37 +0300

    seminar3

---
+++ seminar3/Program.cs
@@ -0,0 +1,27 @@
+﻿// void SayLang(string lang)
+// {
+//     Console.WriteLine($"Hello, {lang} ");
+// }
+
+// SayLang("Python");
+
+// SayLang("C++");
+
+
+
+
+// int MinNum(int num1, int num2)
+// {
+//     if (num1 > num2)
+//     {
+//         return num2 * 2;
+//     }
+//     else{
+//         return num1 *2;
+//     }
+// }
+
+// int num = MinNum(2, 3);
+// Console.WriteLine(num);
+
+// Console.WriteLine(MinNum(5, 1));
